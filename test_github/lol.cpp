d5bb5b2d3e08ddad8f1bbd11998a8de9b21448c9
Chea Bryan <cebeka75@gmail.com>
2019-11-06 00:27:11 +0100

    j'ai cree les deux fichiers

---
+++ test_github/lol.cpp
@@ -0,0 +1,9 @@
+//
+//  lol.cpp
+//  test_github
+//
+//  Created by Bryan Chea on 06/11/2019.
+//  Copyright © 2019 Bryan Chea. All rights reserved.
+//
+
+#include "lol.hpp"
---
+++ test_github/lol.hpp
@@ -0,0 +1,14 @@
+//
+//  lol.hpp
+//  test_github
+//
+//  Created by Bryan Chea on 06/11/2019.
+//  Copyright © 2019 Bryan Chea. All rights reserved.
+//
+
+#ifndef lol_hpp
+#define lol_hpp
+
+#include <stdio.h>
+
+#endif /* lol_hpp */
