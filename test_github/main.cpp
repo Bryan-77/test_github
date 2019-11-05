6530524cef2be755abc1e2e54624f484aa68e2f4
Chea Bryan <cebeka75@gmail.com>
2019-11-06 00:16:11 +0100

    Initial Commit

---
+++ test_github/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test_github
+//
+//  Created by Bryan Chea on 06/11/2019.
+//  Copyright © 2019 Bryan Chea. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
